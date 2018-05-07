3648ea069ec9b7b01525a7cb7bafc4b5d8995046
stamp <stampz74@gmail.com>
2018-05-07 19:03:57 +0700

    Added = to assign type to variables

--- game.go
+++ game.go
@@ -1,13 +1,13 @@
 package lib
 
 // Form to comunicate with plugin
-type Form struct {
+type Form = struct {
 	Header    string
 	Body      string
 	Signature string
 }
 
-type ack func(form Form)
+type ack = func(form Form)
 
 // Game interface engine
 type Game interface {
