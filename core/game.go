f208d9aaa426cd12930bfd8cef33065a986f90a0
stamp <stampz74@gmail.com>
2018-05-21 14:29:11 +0700

    Able to update with ack and remove From

--- core/game.go
+++ core/game.go
@@ -1,22 +1,12 @@
 package core
 
-// Form to comunicate with plugin
-type Form = struct {
-	Header    string
-	Body      string
-	Signature string
-}
-
 // Ack is callback
-type Ack = func(form Form)
+type Ack = func(form string)
 
 // Game interface engine
 type Game interface {
-	On(name string, ack Ack)
 	Init() error
 	Start() error
-	Update(msg []byte) error
-	Pause() error
-	Finish() error
-	Shutdown() error
+	Update(msg []byte, ack Ack) error
+	GetState() ([]byte, error)
 }
